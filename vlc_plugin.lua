04b6346e92c5da95223fc0ccb85a047557618725
eyal1889 <80756636+eyal1889@users.noreply.github.com>
2021-05-13 17:54:55 +0300

    Add files via upload

--- vlc_plugin.lua
+++ vlc_plugin.lua
@@ -2,7 +2,7 @@
  Streaming Radio Player extension for VLC &gt;= 1.1.0
  Authors: Ben Dowling (http://www.coderholic.com)
 --]]
-
+tags= {}
 local dlg2 = nil
 stations = {
     { name = "Ophir the king", url = "http://somafm.com/startstream=groovesalad.pls" },
@@ -13,7 +13,7 @@ function descriptor()
     return { title = "cnsr" ;
              version = "0.1" ;
              author = "EBT" ;
-             capabilities = {} }
+             capabilities = {"menu"} }
 	
 end
 
@@ -31,7 +31,6 @@ function activate()
 		dlg = vlc.dialog("failed: ".. cnsr_name)
 		dlg:show() end
 	io.input(cnsr_file)
-	tags= {}
 	i =0
 	for line in io.lines() do
 		--line = io.read("*line")	
@@ -47,27 +46,26 @@ function activate()
 		end_in_ms = tonumber(h)*3600000 + tonumber(m)*60000 + tonumber(s)*1000 + end_ms
 		
 		timeline = {}
-		timeline[0] = start_in_ms
-		timeline[1] = end_in_ms
-		timeline[2] = typ
+		timeline[0] = tonumber(start_in_ms)
+		timeline[1] = tonumber(end_in_ms)
+		timeline[2] = tonumber(typ)
 		tags[i] = timeline
 		i = i+1
 	end
 	io.close(cnsr_file)
 	cnsr_file = nil
 	collectgarbage()
-	--1. until here we dont know if it works!	
+	--1. until here we dont know if it works!
+	--vlc.misc.mwait(vlc.misc.mdate()+1000000)
+	--vlc.msg.info("paused")
 	dlg2 = vlc.dialog("sencor")
 	checkbox_1 = dlg2:add_check_box("violence",20,2,3,3)
 	checkbox_2 = dlg2:add_check_box("verbal abuse",20,5,3,3)
 	checkbox_3 = dlg2:add_check_box("nudity",20,8,3,3)
 	checkbox_4 = dlg2:add_check_box("alcohol and drug consumption",20,11,3,3)
 	button_apply = dlg2:add_button("apply categories to censor",click_play, 6, 10, 3, 3)
-	--local input = vlc.object.input()
-	--local current_time = vlc.var.get(input,"time")
-	--button_apply = dlg2:add_button("h",click_play, 6, 10, 3, 3)
+	--button_apply = dlg2:add_button(tags[0][2],click_play, 6, 10, 3, 3)
     dlg2:show()
-		
 	
 end
 
@@ -81,42 +79,48 @@ function unescape (s)
 
 
 function click_play()
-type1= checkbox_1:get_checked()
+	type1= checkbox_1:get_checked()
 	type2= checkbox_2:get_checked()
 	type3= checkbox_3:get_checked()
 	type4= checkbox_4:get_checked()
 	close_dlg()
+	i=0
+	did_jump=0
+	local duration = vlc.input.item():duration()*1000
 	while true do
-		vlc.misc.mwait(vlc.misc.mdate() + 1000)
-
+		local input = vlc.object.input()
+		local current_time = vlc.var.get(input,"time")/1000
+		next_tag_start = tags[i][0]
+		next_end_time=tags[i][1]
+		typ = tags[i][2]
+		if (current_time< next_tag_start or current_time> next_end_time) then
+			j=0
+		else
+			if (type1==true and typ ==1 and did_jump==0) then
+				
+				vlc.var.set(input,"position",next_end_time/duration)
+				did_jump=1
+			end
+	--		if (type2==true and tags[i].typ==2) then
+	--			vlc.var.set(input,"position",next_end_time/duration)
+	--		end
+	--		if (type3==true tags[i].typ==3) then
+	--			vlc.var.set(input,"position",next_end_time/duration)
+	--		end
+	--		if (type4==true tags[i].typ==4) then
+	--			vlc.var.set(input,"position",next_end_time/duration)
+	--		end
+		end
 	end
 	
-	if (type1==true) then
-		local input = vlc.object.input()
-		local current_time = vlc.var.get(input,"position")
-		local duration = vlc.input.item():duration()
-		vlc.var.set(input,"position",70/duration)
-	end
-	if (type2==true) then
-		local input = vlc.object.input()
-		local current_time = vlc.var.get(input,"position")
-		local duration = vlc.input.item():duration()
-		vlc.var.set(input,"position",700/duration)
-	end
-	if (type3==true) then
-		local input = vlc.object.input()
-		local current_time = vlc.var.get(input,"position")
-		local duration = vlc.input.item():duration()
-		vlc.var.set(input,"position",5000/duration)
-	end
-	if (type4==true) then
-		local input = vlc.object.input()
-		local current_time = vlc.var.get(input,"position")
-		local duration = vlc.input.item():duration()
-		vlc.var.set(input,"position",7000/duration)
-	end
     
 end
+
+function sleep(s)
+  local ntime = os.time() + s
+  repeat until os.time() > ntime
+end
+
 
 -- Function triggered when the extension is deactivated
 function deactivate()
