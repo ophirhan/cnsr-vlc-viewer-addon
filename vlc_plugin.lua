edcdb62481ff6708d82dbcc877f2fca0f9db74ef
ophirhan <67260757+ophirhan@users.noreply.github.com>
2021-05-16 17:22:13 +0300

    Shows skipped <reason> toast.

--- vlc_plugin.lua
+++ vlc_plugin.lua
@@ -1,11 +1,15 @@
 local dlg2 = nil
-types= {}
+categories = {[1] = {description = "violence"},
+			[2] = {description = "verbal abuse"},
+			[3] =  {description = "nudity"},
+			[4] =  {description = "alcohol and drug consumption"}} --add epilepsy category
 
 function descriptor()
     return { title = "cnsr" ;
              version = "0.1" ;
              author = "EEO" ;
-             capabilities = {"menu","playing-listener", "input-listener"} }
+             capabilities = {"playing-listener", "input-listener"};
+			 url =  "https://github.com/ophirhan/cnsr-vlc-viewer-addon"}
 	
 end
 
@@ -15,10 +19,10 @@ function strip_extension(uri)
 	return string.sub(uri, 0, i)
 end
 
-function hms_ms_to_ms(time_string)
+function hms_ms_to_us(time_string) -- microseconds
 		hms , ms = string.match(time_string, "([^,]+),([^,]+)") -- maybe use find and sub instead of slow regex
 		h, m ,s = string.match(hms, "([^:]+):([^:]+):([^:]+)")
-		return tonumber(h)*3600000 + tonumber(m)*60000 + tonumber(s)*1000 + tonumber(ms)
+		return (tonumber(h)*3600000 + tonumber(m)*60000 + tonumber(s)*1000 + tonumber(ms)) * 1000
 end
 
 function load_cnsr_file()
@@ -27,7 +31,6 @@ function load_cnsr_file()
 		return
 	end
 	tags = {}
-	duration_ms = vlc.input.item():duration() * 1000
 	tag_index = 1
 	local uri = vlc.input.item():uri()
 	uri = unescape(uri)
@@ -41,19 +44,18 @@ function load_cnsr_file()
 	end
 	io.input(cnsr_file)
 	
-	local i = 1
 	for line in io.lines() do
 		line = string.gsub(line," ","")
 		local times, typ = string.match(line,"([^;]+);([^;]+)") -- maybe use find and sub instead of slow regex
 		local start_time, end_time = string.match(times,"([^-]+)-([^-]+)")
 		typ = tonumber(typ)
 		
-		if types[typ] then
+		if categories[typ].censor then
 			timeline = {}
-			timeline["start"] = hms_ms_to_ms(start_time)
-			timeline["end"] = hms_ms_to_ms(end_time)
-			tags[i] = timeline
-			i = i+1
+			timeline.start_time = hms_ms_to_us(start_time)
+			timeline.end_time = hms_ms_to_us(end_time)
+			timeline.category = typ
+			table.insert(tags, timeline)
 		end
 	end
 	io.close(cnsr_file)
@@ -65,16 +67,16 @@ end
 -- Function triggered when the extension is activated
 function activate()
 	dlg2 = vlc.dialog("censor")
-	checkbox_1 = dlg2:add_check_box("violence",20,2,3,3)
-	checkbox_2 = dlg2:add_check_box("verbal abuse",20,5,3,3)
-	checkbox_3 = dlg2:add_check_box("nudity",20,8,3,3)
-	checkbox_4 = dlg2:add_check_box("alcohol and drug consumption",20,11,3,3)
+	local pos = 2
+	for i,v in ipairs(categories) do
+		v.checkbox = dlg2:add_check_box(v.description,20,pos,3,3)
+		pos = pos + 3
+	end
 	button_apply = dlg2:add_button("apply categories to censor",click_play, 6, 10, 3, 3)
     dlg2:show()
-	
 end
 
-function unescape (s)
+function unescape (s) --replace with vlc.strings.decode_uri()
 	s = string.gsub(s, "+", " ")
 	s = string.gsub(s, "%%(%x%x)", 
 		function (h)
@@ -85,11 +87,10 @@ end
 
 
 function click_play()
-	types[1] = checkbox_1:get_checked()
-	types[2] = checkbox_2:get_checked()
-	types[3] = checkbox_3:get_checked()
-	types[4] = checkbox_4:get_checked()
-	close_dlg()
+	for i,v in ipairs(categories) do
+		v.censor = v.checkbox:get_checked()
+	end
+	close_dlg() --add option to reopen dialog and reload tags according to new filters
 	load_cnsr_file()
 	loop()
 end
@@ -110,19 +111,22 @@ function loop()
 		end
 		
 		--find right tag_index
-		local current_time = vlc.var.get(input,"time") / 1000
-		while tag_index > 1 and current_time < tags[tag_index-1]["end"] do --while current_time < previous tag end time
+		local current_time = vlc.var.get(input,"time")
+		while tag_index > 1 and current_time < tags[tag_index-1].end_time do --while current_time < previous tag end time
 			tag_index = tag_index - 1
 		end
-		while tag_index < #tags and current_time > tags[tag_index]["end"] do --while current_time > current tag end time?
+		while tag_index < #tags and current_time > tags[tag_index].end_time do --while current_time > current tag end time?
 			tag_index = tag_index + 1 --both while loops not thought out, what about collisions, maybe sort by end time?
 		end
 		
-		local next_tag_start = tags[tag_index]["start"]
-		local next_end_time = tags[tag_index]["end"]
-		
-		if (current_time > next_tag_start and current_time < next_end_time) then
-			vlc.var.set(input,"position",(next_end_time+1) / duration_ms)
+		local next_tag_start = tags[tag_index].start_time
+		local next_end_time = tags[tag_index].end_time
+		local category = tags[tag_index].category
+
+		--osd.slider( position, type, [id] ): Display slider. Position is an integer from 0 to 100. Type can be "horizontal" or "vertical".
+		if (current_time > next_tag_start and current_time < next_end_time) then -- maybe add slider leading to skip?
+			vlc.var.set(input,"time", next_end_time + 10000) --add option to mute
+			vlc.osd.message("skipped " .. categories[category].description, nil, "bottom-right") --what about collisions? add how many seconds were skipped?
 		end
 	end
 end
@@ -135,6 +139,8 @@ end
 function playing_changed()
 	loop()
 end
+
+--lua README titles to explore: "Objects (player, libvlc, vout), Renderer discovery"
 
 
 -- Function triggered when the extension is deactivated
