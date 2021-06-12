14bcb899f6799d52dd4dd26b1cc44062ac0991cf
ophirhan <67260757+ophirhan@users.noreply.github.com>
2021-06-12 17:14:11 +0300

    Revert "intf refactoring - logic, code conventions, var scope and styling"

--- intf/cnsr_intf.lua
+++ intf/cnsr_intf.lua
@@ -1,7 +1,6 @@
 
 os.setlocale("C", "all") -- fixes numeric locale issue on Mac
 
--- constants
 config={}
 SHOW = 1
 SKIP = 2
@@ -11,7 +10,6 @@ FRAME_INTERVAL = 30000
 SKIP_SAFETY = 10000
 MINIMUM_DISPLAY_TIME = 2000000
 GET_CONFIG_INTERVAL = 500
--- end constants
 
 
 DESCRIPTIONS = { [1] = "violence",
@@ -19,64 +17,62 @@ DESCRIPTIONS = { [1] = "violence",
 			   [3] =  "nudity",
 			   [4] =  "alcohol and drug consumption"}
 
--- globals
+
+tags = {}
 tag_index = 1
 current_time = 0
 prev_time = 0
+loop_counter = 0
 reverse = false
 done = false
--- end globals
 
--- mute
-MuteParams = {
-	start_time = 0,
-	end_time = 0,
-	activated = false,
-	prev_volume = vlc.volume.get(),
-	action_word = "muted"
-}
+mute_params = {}
+mute_params.start_time = 0
+mute_params.end_time = 0
+mute_params.activated = false
+mute_params.prev_volume = vlc.volume.get()
+mute_params.action_word = "muted"
 
-function MuteParams.mute()
-	MuteParams.prev_volume = vlc.volume.get()
+function mute()
+	mute_params.prev_volume = vlc.volume.get()
 	vlc.volume.set(0)
 end
--- end mute
 
--- hide
-HideParams = {
-	start_time = 0,
-	end_time = 0,
-	activated = false,
-	action_word = "hidden"
-}
+mute_params.activate = mute
 
-function HideParams.activate()
+hide_params = {}
+hide_params.start_time = 0
+hide_params.end_time = 0
+hide_params.activated = false
+hide_params.action_word = "hidden"
+
+function hide()
 	hide_filter = vlc.object.vout()
 	vlc.var.create(hide_filter, "contrast", 0)
 	vlc.var.set(hide_filter, "video-filter", "adjust")
 end
--- end hide
 
--- Main app entry point
-function looper()
-	local next_loop_time
-	local loop_counter = 0
-	local tags;
+hide_params.activate = hide
+
+
+
+function Looper()
+	next_loop_time = vlc.misc.mdate()
 	while true do
 		if vlc.volume.get() == -256 then break end  -- inspired by syncplay.lua; kills vlc.exe process in Task Manager
 		if loop_counter == 0 then
-			get_config() -- We don't want to call it more then we have to.
-			log("got config")
+			Get_config() -- We don't want to call it more then we have to.
+			Log("got config")
 		end
 		loop_counter = (loop_counter + 1) % GET_CONFIG_INTERVAL
 		if vlc.playlist.status()~="stopped" and config.CNSR and config.CNSR.tags and #config.CNSR.tags ~= 0 then
 			tags = config.CNSR.tags
 
-			local input = vlc.object.input()
+			input = vlc.object.input()
 			current_time = vlc.var.get(input,"time")
 			check_disable_actions()
 			reverse = prev_time > current_time
-			local tag = get_current_tag()
+			tag = get_current_tag()
 
 			while not done and current_time > tag.start_time do
 				if current_time < tag.end_time and tag.action ~= SHOW then
@@ -102,28 +98,28 @@ function display_reason(reason_string, category, tag_end)
 end
 
 function check_disable_actions()
-	if MuteParams.activated and (current_time > MuteParams.end_time or current_time < MuteParams.start_time) then
-		vlc.volume.set(MuteParams.prev_volume)
-		MuteParams.activated = false
+	if mute_params.activated and (current_time > mute_params.end_time or current_time < mute_params.start_time) then
+		vlc.volume.set(mute_params.prev_volume)
+		mute_params.activated = false
 	end
 
-	if HideParams.activated and (current_time > HideParams.end_time or current_time < HideParams.start_time) then
+	if hide_params.activated and (current_time > hide_params.end_time or current_time < hide_params.start_time) then
 		vlc.var.set(hide_filter, "video-filter", "")
-		HideParams.activated = false
+		hide_params.activated = false
 	end
 end
 
 function skip(skip_start, skip_end)
-	local current_time = skip_end + SKIP_SAFETY
-	if reverse then -- we went back in time, cut the duration of skip tag from timeline
-		local skip_length = skip_end - skip_start
+	if not reverse then
+		current_time = skip_end + SKIP_SAFETY
+	else -- we went back in time, cut the duration of skip tag from timeline
+		skip_length = skip_end - skip_start
 		current_time = math.max(current_time - skip_length, 0)
 	end
 	vlc.var.set(input,"time", current_time)
 end
 
-function get_current_tag(tags)
-	-- todo remove the commented code block
+function get_current_tag()
 	--[[
 	if reverse then
 		relevant_tags = #[tag where (current_time < tag.end_time) in tags] -> sort tags by end time once-> binary search starting at tag_index
@@ -150,23 +146,23 @@ function get_current_tag(tags)
 end
 
 function check_collision()
-	if HideParams.activated and MuteParams.activated then
-		local skip_end = math.min(MuteParams.end_time, HideParams.end_time)
-		local skip_start = math.max(MuteParams.start_time, HideParams.start_time)
+	if hide_params.activated and mute_params.activated then
+		local skip_end = math.min(mute_params.end_time, hide_params.end_time)
+		local skip_start = math.max(mute_params.start_time, hide_params.start_time)
 		skip(skip_start, skip_end)
 	end
 end
 
 function execute_tag(tag)
-	local action_params
+
 	if tag.action == SKIP then
 		skip(tag.start_time, tag.end_time)
 		display_reason("skipped", tag.category, tag.end_time) --add how many seconds were skipped?
 	else
 		if tag.action == MUTE then
-			action_params = MuteParams
+			action_params = mute_params
 		elseif tag.action == HIDE then
-			action_params = HideParams
+			action_params = hide_params
 		else
 			vlc.msg.err("unknown action")
 		end
@@ -184,15 +180,15 @@ function execute_tag(tag)
 	end
 end
 
-function log(lm)
+function Log(lm)
 	vlc.msg.info("[cnsr_intf] " .. lm)
 end
 
-function get_config()
+function Get_config()
 	local s = vlc.config.get("bookmark10")
 	if not s or not string.match(s, "^config={.*}$") then s = "config={}" end
 	assert(loadstring(s))() -- global var
 end
 
 
-looper() --starter
+Looper() --starter
