9b71360593f25c4e00a4f15828b060b89026a0b6
ophirhan <67260757+ophirhan@users.noreply.github.com>
2021-06-16 00:22:55 +0300

    Make script extensible to new actions.

--- intf/cnsr_intf.lua
+++ intf/cnsr_intf.lua
@@ -3,22 +3,16 @@ os.setlocale("C", "all") -- fixes numeric locale issue on Mac
 
 -- constants
 config={}
-SHOW = 1
-SKIP = 2
-MUTE = 3
-HIDE = 4
 MS_IN_SEC =1000000
 FRAME_INTERVAL = 30000
 SKIP_SAFETY = 10000
 MINIMUM_DISPLAY_TIME = 2000000
 GET_CONFIG_INTERVAL = 500
--- end constants
-
-
 DESCRIPTIONS = { [1] = "violence",
 				 [2] = "verbal abuse",
 				 [3] =  "nudity",
 				 [4] =  "alcohol and drug consumption"}
+-- end constants
 
 -- globals
 tag_index = 1
@@ -27,38 +21,96 @@ current_time = 0
 prev_time = 0
 reverse = false
 done = false
+input = nil
+actions = {}
 -- end globals
 
--- mute
-MuteParams = {
-	start_time = 0,
-	end_time = 0,
-	activated = false,
-	prev_volume = vlc.volume.get(),
-	action_word = "muted"
-}
-
-function MuteParams.activate()
-	MuteParams.prev_volume = vlc.volume.get()
-	vlc.volume.set(0)
+function nothing()
 end
+
+
+-- show
+actions.show = { execute= nothing, update= nothing }
+
+table.insert(actions, actions.show) --1
+-- end show
+
+-- skip
+actions.skip = {}
+
+function actions.skip.execute(tag)
+	skip(tag.start_time, tag.end_time)
+	display_reason("skipped", tag.category, tag.end_time)
+end
+
+actions.skip.update = nothing;
+
+table.insert(actions, actions.skip) --2
+-- end skip
+
+function check_activate(action, tag)
+	if action.activated then
+		action.end_time = math.max(action.end_time, tag.end_time)
+	else
+		action.start_time = tag.start_time
+		action.end_time = tag.end_time
+		action.activated = true
+		action.activate()
+	end
+	display_reason(action.action_word, tag.category, tag.end_time)
+end
+
+function check_deactivate(action)
+	if action.activated and (current_time > action.end_time or current_time < action.start_time) then
+		action.deactivate()
+		action.activated = false
+	end
+end
+
+-- mute
+actions.mute = { activated = false, action_word = "muted" }
+
+actions.mute.execute= function(tag)	check_activate(actions.mute, tag) end
+
+actions.mute.update= function() check_deactivate(actions.mute) end
+
+function actions.mute.activate()
+	actions.mute.prev_volume = vlc.volume.get()
+	vlc.volume.set(0)
+	check_collision()
+end
+
+actions.mute.deactivate= function() vlc.volume.set(actions.mute.prev_volume) end
+table.insert(actions, actions.mute) --3
 -- end mute
 
 -- hide
-HideParams = {
-	start_time = 0,
-	end_time = 0,
-	activated = false,
-	action_word = "hidden"
-}
+actions.hide = { activated = false, action_word = "hidden" }
 
+actions.hide.execute= function(tag) check_activate(actions.hide, tag) end
 
-function HideParams.activate()
-	hide_filter = vlc.object.vout()
-	vlc.var.create(hide_filter, "contrast", 0)
-	vlc.var.set(hide_filter, "video-filter", "adjust")
+actions.hide.update= function() check_deactivate(actions.hide) end
+
+function actions.hide.activate()
+	actions.hide.hide_filter = vlc.object.vout()
+	vlc.var.create(actions.hide.hide_filter, "contrast", 0)
+	vlc.var.set(actions.hide.hide_filter, "video-filter", "adjust")
+	check_collision()
 end
+
+actions.hide.deactivate= function() vlc.var.set(actions.hide.hide_filter, "video-filter", "") end
+
+table.insert(actions, actions.hide) --4
 -- end hide
+
+
+function check_collision()
+	if actions.hide.activated and actions.mute.activated then
+		local skip_end = math.min(actions.mute.end_time, actions.hide.end_time)
+		local skip_start = math.max(actions.mute.start_time, actions.hide.start_time)
+		skip(skip_start, skip_end)
+	end
+end
 
 -- Main app entry point
 function looper()
@@ -74,15 +126,15 @@ function looper()
 			local tags = config.CNSR.tags
 			local tags_by_end_time = config.CNSR.tags_by_end_time
 
-			local input = vlc.object.input()
+			input = vlc.object.input()
 			current_time = vlc.var.get(input,"time")
-			check_disable_actions()
+			update_actions()
 			reverse = prev_time > current_time
 			local tag = get_current_tag(tags, tags_by_end_time)
 
 			while not done and current_time > tag.start_time do
 				if current_time < tag.end_time and tag.action ~= SHOW then
-					execute_tag(tag, input)
+					actions[tag.action].execute(tag)
 				end
 				done = tag_index == #tags
 				if not done and current_time>tag.start_time then
@@ -97,30 +149,22 @@ function looper()
 	end
 end
 
+function update_actions()
+	for _, action in ipairs(actions) do
+		action.update()
+	end
+end
+
 function display_reason(reason_string, category, tag_end)
 	if tag_end - current_time > MINIMUM_DISPLAY_TIME and not reverse then
 		vlc.osd.message(reason_string .. " " .. DESCRIPTIONS[category], nil, "bottom-right")
 	end
 end
 
-function check_disable_actions()
-	if MuteParams.activated and (current_time > MuteParams.end_time or current_time < MuteParams.start_time) then
-		vlc.volume.set(MuteParams.prev_volume)
-		MuteParams.activated = false
-	end
-
-	if HideParams.activated and (current_time > HideParams.end_time or current_time < HideParams.start_time) then
-		vlc.var.set(hide_filter, "video-filter", "")
-		HideParams.activated = false
-	end
-end
-
-function skip(skip_start, skip_end, input)
+function skip(skip_start, skip_end)
 	local skip_length = skip_end - skip_start
 	if reverse then -- we went back in time, cut the duration of skip tag from timeline
 		current_time = math.max(current_time - skip_length, 0)
-		--log(tostring(current_time/1000000))
-		--log("tag: "..tostring(tag_index))
 	else
 		current_time = math.min(current_time + skip_length + SKIP_SAFETY,vlc.input.item():duration()*MS_IN_SEC) --think if we want to!
 	end
@@ -147,20 +191,17 @@ end
 function get_current_tag(tags, tags_by_end_time)
 	if reverse then
 		relevant_tags = get_num_relevant_tags(tags_by_end_time) --#(tags where current_time < tag.end_time)
-		log("relevant tags" .. tostring(relevant_tags))
 
 		relevant_tags_after_index = #tags - tag_index
 		if current_time < tags[tag_index].end_time  then
 			relevant_tags_after_index = relevant_tags_after_index + 1
 		end
-		log("relevant_tags_after_index before loop" .. tostring(relevant_tags_after_index))
 		while relevant_tags_after_index < relevant_tags do
 			tag_index = tag_index - 1
 			if current_time < tags[tag_index].end_time  then
 				relevant_tags_after_index = relevant_tags_after_index + 1
 			end
 		end
-		log("after reverse" .. tostring(tag_index))
 		done = false
 	end
 	while tag_index < #tags and current_time > tags[tag_index].end_time do
@@ -169,43 +210,9 @@ function get_current_tag(tags, tags_by_end_time)
 	return tags[tag_index]
 end
 
-function check_collision(input)
-	if HideParams.activated and MuteParams.activated then
-		local skip_end = math.min(MuteParams.end_time, HideParams.end_time)
-		local skip_start = math.max(MuteParams.start_time, HideParams.start_time)
-		skip(skip_start, skip_end, input)
-	end
-end
-
-function execute_tag(tag, input)
-	local action_params
-	if tag.action == SKIP then
-		skip(tag.start_time, tag.end_time, input)
-		display_reason("skipped", tag.category, tag.end_time) --add how many seconds were skipped?
-	else
-		if tag.action == MUTE then
-			action_params = MuteParams
-		elseif tag.action == HIDE then
-			action_params = HideParams
-		else
-			vlc.msg.err("unknown action")
-		end
-
-		if not action_params.activated then
-			action_params.start_time = tag.start_time
-			action_params.end_time = tag.end_time
-			action_params.activated = true
-			action_params.activate()
-			check_collision(input)
-		else
-			action_params.end_time = math.max(action_params.end_time, tag.end_time)
-		end
-		display_reason(action_params.action_word, tag.category, tag.end_time)
-	end
-end
-
-function log(lm)
-	vlc.msg.info("[cnsr_intf] " .. lm)
+function log(str,num)
+	if num then str = str .. " " .. tostring(num) end
+	vlc.msg.info("[cnsr_intf] " .. str)
 end
 
 function get_config()
